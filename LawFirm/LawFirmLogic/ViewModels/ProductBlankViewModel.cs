c457b387d07dbf5cc7fec2711b6a2454675c8733
helencruel228@outlook.com <helencruel228@outlook.com>
2020-06-04 16:51:11 +0300

    8

---
+++ LawFirm/LawFirmLogic/ViewModels/ProductBlankViewModel.cs
@@ -0,0 +1,33 @@
+﻿using LawFirmLogic.Attributes;
+using LawFirmLogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace LawFirmLogic.ViewModels
+{
+    [DataContract]
+    public class ProductBlankViewModel : BaseViewModel
+    {
+        [DataMember]
+        public int ProductId { get; set; }
+
+        [DataMember]
+        public int BlankId { get; set; }
+
+        [DataMember]
+        [Column(title: "Бланк", gridViewAutoSize: GridViewAutoSize.Fill)]
+        public string BlankName { get; set; }
+
+        [DataMember]
+        [Column(title: "Количество", width: 100)]
+        public int Count { get; set; }
+        public override List<string> Properties() => new List<string>
+        {
+            "Id",
+            "BlankName",
+            "Count"
+        };
+    }
+}
