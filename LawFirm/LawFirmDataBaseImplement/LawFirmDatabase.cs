401f1bc1664f7248fb53140937b1659c1d394193
helencruel228@outlook.com <helencruel228@outlook.com>
2020-06-26 19:47:25 +0300

    fix

--- LawFirm/LawFirmDataBaseImplement/LawFirmDatabase.cs
+++ LawFirm/LawFirmDataBaseImplement/LawFirmDatabase.cs
@@ -12,7 +12,7 @@ namespace LawFirmDataBaseImplement
         {
             if (optionsBuilder.IsConfigured == false)
             {
-                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-1L0DP37\SQLEXPRESS;Initial Catalog=LawFirmDatabaseHomework;Integrated Security=True;MultipleActiveResultSets=True;");
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-1L0DP37;Initial Catalog=LawFirmDatabaseHomework;Integrated Security=True;MultipleActiveResultSets=True;");
             }
             base.OnConfiguring(optionsBuilder);
         }
--- LawFirm/LawFirmDataBaseImplement/Migrations/20200626162543_InitialCreate.Designer.cs
+++ LawFirm/LawFirmDataBaseImplement/Migrations/20200626162543_InitialCreate.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LawFirmDataBaseImplement.Migrations
 {
     [DbContext(typeof(LawFirmDatabase))]
-    [Migration("20200607121652_InitialCreate")]
+    [Migration("20200626162543_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.3")
+                .HasAnnotation("ProductVersion", "3.1.4")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
